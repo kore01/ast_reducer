53f6ce3091d73cc38cb7ddc5dec542bf2b7a2b65
mvieira00 <100200252+mvieira00@users.noreply.github.com>
2025-06-08 18:59:05 +0200

    Update remove_redundant.py

--- remove_redundant.py
+++ remove_redundant.py
@@ -12,6 +12,7 @@ from sqlglot.errors import TokenError, ParseError
 import tempfile
 
 from remove_redundant_parentheses import remove_redundant_parentheses
+from delta_reduce_single_statements import test_for_fail
 
 def remove_redundant(sql_queries_dir: Path, test_script: Path) -> str:
     curr_sql = ""
@@ -50,34 +51,6 @@ def remove_redundant(sql_queries_dir: Path, test_script: Path) -> str:
         else:
             post_next_sql = next_sql + post_next_sql
     
-    print(f"Reduced query: {post_next_sql}")
+    #print(f"Reduced query: {post_next_sql}")
     return post_next_sql
 
-
-@lru_cache(maxsize=14124)
-def test_for_fail(sql_query: str, test_script: Path, pre_next_sql: str, post_next_sql: str) -> int:
-    curr_query = pre_next_sql + post_next_sql
-
-    try:
-        with tempfile.NamedTemporaryFile(mode="w+", suffix=".sql", delete=False) as tmp_file:
-            tmp_file.write(curr_query)
-            tmp_file.flush()
-            tmp_path = Path(tmp_file.name)
-
-            # Use tmp_path in your subprocess
-            result = subprocess.run(
-                [str(test_script), str(tmp_path)],
-                capture_output=True,
-                text=True,
-                check=False
-            )
-            tmp_path.unlink()
-        return result.returncode
-    except subprocess.CalledProcessError as e:
-        print(f"Script failed with return code {e.returncode}")
-        print(f"stderr: {e.stderr}")
-        raise
-    except ValueError as e:
-        print("Failed to convert output to int")
-        print(f"Output was: {result.stdout}")
-        raise
