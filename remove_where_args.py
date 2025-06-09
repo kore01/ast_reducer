57818880ebcffe1aea8b97969d2c50131deadfee
mvieira00 <100200252+mvieira00@users.noreply.github.com>
2025-06-09 17:48:40 +0200

    Update remove_where_args.py
    commented a "print" out at the end

--- remove_where_args.py
+++ remove_where_args.py
@@ -62,4 +62,4 @@ def remove_where_args(sql: str, index: int) -> str:
     return tree.sql(pretty=False, dialect='sqlite').replace("SELECT *", "SELECT*")
 
 
-print(remove_where_args("INSERT INTO F SELECT * FROM (VALUES ((NOT false), false), (NULL, (NOT (NOT true)))) AS L WHERE (((+(+(-((+110) / (+((-(-150)) * ((247 * (91 * (-47))) + (-86)))))))) = ((((+(+(24 / (+((+89) * (+58)))))) * (-(-((193 + 223) / (-(222 / 219)))))) * (34 * 70)) * (+(+((((+(+(-202))) / (+52)) - (-(228 + (-104)))) * (-24)))))) = (false <> (66 <> 8)));", -1))
+#print(remove_where_args("INSERT INTO F SELECT * FROM (VALUES ((NOT false), false), (NULL, (NOT (NOT true)))) AS L WHERE (((+(+(-((+110) / (+((-(-150)) * ((247 * (91 * (-47))) + (-86)))))))) = ((((+(+(24 / (+((+89) * (+58)))))) * (-(-((193 + 223) / (-(222 / 219)))))) * (34 * 70)) * (+(+((((+(+(-202))) / (+52)) - (-(228 + (-104)))) * (-24)))))) = (false <> (66 <> 8)));", -1))
